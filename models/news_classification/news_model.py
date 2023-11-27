b341ae922e4750f2943400c749047b848b640ad0
Jasmin Krenz <krenzj@cip.ifi.lmu.de>
2023-11-27 20:31:14 +0100

    create labels

--- models/news_classification/news_model.py
+++ models/news_classification/news_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import LabelEncoder
 import json
 from text_processing import create_sparse_vectors
 
@@ -14,11 +15,25 @@ def load_data(file_path):
     return data
 
 
-path = '../../data/classification_data/data/news/classification/classification_news_train.jsonl'
-news_data = load_data(file_path=path)
+def create_labels(loaded_data):
+    labels = [entry['category'] for entry in loaded_data]
+    label_encoder = LabelEncoder()
 
-X_sparse = create_sparse_vectors(news_data)
-print(X_sparse)
+    # fits the encoder to the unique labels in your data and transforms the labels into numerical values
+    # each color is assigned an integer
+    y = label_encoder.fit_transform(labels)
+    y_onehot = torch.nn.functional.one_hot(torch.tensor(y))
 
-#labels = [entry['category'] for entry in news_data]
+    label_mapping = dict(zip(label_encoder.classes_, y_onehot.numpy()))
+    return y_onehot, label_mapping
 
+
+if __name__ == "__main__":
+    path = '../../data/classification_data/data/news/classification/classification_news_train.jsonl'
+    news_data = load_data(file_path=path)
+
+    #X_sparse = create_sparse_vectors(news_data)
+    #print(X_sparse)
+
+    labels_onehot, labels_mapping = create_labels(news_data)
+    print(labels_mapping)
