90941e1f1e776dc89c66ddc94633763e3fc882b6
Jasmin Krenz <krenzj@cip.ifi.lmu.de>
2023-11-28 17:14:57 +0100

    training fnn

--- models/news_classification/news_model.py
+++ models/news_classification/news_model.py
@@ -22,18 +22,75 @@ def create_labels(loaded_data):
     # fits the encoder to the unique labels in your data and transforms the labels into numerical values
     # each color is assigned an integer
     y = label_encoder.fit_transform(labels)
+
+    # one hot encoding for nominal data
     y_onehot = torch.nn.functional.one_hot(torch.tensor(y))
 
     label_mapping = dict(zip(label_encoder.classes_, y_onehot.numpy()))
-    return y_onehot, label_mapping
+    return y_onehot, label_mapping, len(label_encoder.classes_)
+
+
+class FeedforwardNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(FeedforwardNN, self).__init__()
+        self.fully_connected_1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fully_connected_2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        x = self.fully_connected_1(x)  # Linear transformation with weights and biases
+        x = self.relu(x)
+        x = self.fully_connected_2(x)
+        return x
 
 
 if __name__ == "__main__":
     path = '../../data/classification_data/data/news/classification/classification_news_train.jsonl'
+    path_testset = '../../data/classification_data/data/news/classification/classification_news_eval.jsonl'
+
     news_data = load_data(file_path=path)
+    #news_data_test = load_data(file_path=path_testset)
 
-    #X_sparse = create_sparse_vectors(news_data)
-    #print(X_sparse)
+    X_sparse_train = create_sparse_vectors(news_data)
+    print(X_sparse_train)
 
-    labels_onehot, labels_mapping = create_labels(news_data)
+    labels_onehot, labels_mapping, count_labels = create_labels(news_data)
     print(labels_mapping)
+
+    input_size = X_sparse_train.shape[1]
+    hidden_size = 64
+    output_size = count_labels
+
+    # Instantiate the model
+    model = FeedforwardNN(input_size, hidden_size, output_size)
+
+    # Loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # Training the model
+    num_epochs = 10
+    batch_size = 64
+
+    # Convert sparse matrix to PyTorch tensor
+    X_train_tensor = torch.tensor(X_sparse_train.toarray(), dtype=torch.float32)
+    # Create a DataLoader for training
+    train_dataset = TensorDataset(X_train_tensor, labels_onehot)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    # Training
+    for epoch in range(num_epochs):
+        for batch_x, batch_y in train_loader:
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(batch_x)
+
+            loss = criterion(outputs, torch.argmax(batch_y, dim=1))
+            loss.backward()
+            optimizer.step()
+
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item()}')
+
+# Save the trained model
+torch.save(model.state_dict(), 'genre_classification_model.pth')
