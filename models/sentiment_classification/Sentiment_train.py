28cb99807724ab123936d5bc6c61b1eaa61e00ba
yuchen <viveutvivasmyc@gmail.com>
2023-12-05 00:13:27 +0100

    1. change the activation function between hidden layers into LeakyReLU, so that loss reduces during training. 2. move sparse_to_Tensor function into NNModel File.

--- models/sentiment_classification/Sentiment_train.py
+++ models/sentiment_classification/Sentiment_train.py
@@ -1,15 +1,13 @@
 import sentiment_NNModel
 import sentiment_preprocessing
-from sentiment_NNModel import accuracy, FFNN, train_model
+from sentiment_NNModel import accuracy, sparse_to_Tensor, FFNN, train_model
 from sentiment_preprocessing import get_matrix_for_models, load_dataset
-
 import torch
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import torch.nn as nn
 from torch import optim
-
 
 
 """load the dataset"""
@@ -21,16 +19,6 @@ test_data = load_dataset(test_path)
 # hyper_param = {'max_df': 0.6, 'min_df': 0.005}
 # convert the dataset to tensor, and split the training data into train and validation set
 x_train, y_train, x_test, y_test = get_matrix_for_models(train_data, test_data)
-# X_train, X_val, y_train, y_val = train_test_split(X_training, y_training, test_size=0.2)
-
-"""convert the sparse matrix and label lists into pytorch tensor"""
-def sparse_to_Tensor(sparse_features):
-    sparse_tensor = torch.sparse_coo_tensor(
-        torch.LongTensor(np.vstack(sparse_features.nonzero())),
-        torch.FloatTensor(sparse_features.data),
-        torch.Size(sparse_features.shape),
-    )
-    return sparse_tensor
 
 device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
 
@@ -46,26 +34,27 @@ Y_test = torch.tensor(test_encoded).to(device)
 
 
 """train the model with training data"""
-# hyper-perameters
-hidden_sizes = [128, 64, 32]
-batch_size = 32
-learning_rate = 0.001
-num_epochs = 1000
-# parameters
 input_size = X_train.shape[1]
 output_size = 2
+# hyper-perameters for model
+hidden_sizes = [128, 64, 32]
+# hyper-perameters for training
+batch_size = 32
+learning_rate = 0.01
+num_epochs = 1000
 
-fn = FFNN(input_size, hidden_sizes, output_size)
-loss_fn = nn.CrossEntropyLoss()
-opt = optim.SGD(fn.parameters(), lr=learning_rate)
+model = FFNN(input_size=input_size, output_size=output_size, hidden_sizes=hidden_sizes)
 
-print('Final loss', train_model(X_train, Y_train, fn, opt, loss_fn, num_epochs))
+# Define Loss, Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
+train_model(X_train, Y_train, model, optimizer, criterion, batch_size, num_epochs)
 
 
 """check the accuracy on training and test data"""
-Y_pred_train = fn.predict(X_train)
-Y_pred_test = fn.predict(X_test)
+Y_pred_train = model.predict(X_train)
+Y_pred_test = model.predict(X_test)
 
 accuracy_train = accuracy(Y_pred_train, Y_train)
 accuracy_val = accuracy(Y_pred_test, Y_test)
--- models/sentiment_classification/sentiment_NNModel.py
+++ models/sentiment_classification/sentiment_NNModel.py
@@ -10,11 +10,7 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 
-
-
 print("GPU Available:", torch.cuda.is_available())
-
-
 
 def accuracy(y_hat, y):
     """function to calculate accuracy"""
@@ -22,35 +18,38 @@ def accuracy(y_hat, y):
     return (pred == y).float().mean()
 
 
+def sparse_to_Tensor(sparse_features):
+    """convert the sparse matrix and label lists into pytorch tensor"""
+    sparse_tensor = torch.sparse_coo_tensor(
+        torch.LongTensor(np.vstack(sparse_features.nonzero())),
+        torch.FloatTensor(sparse_features.data),
+        torch.Size(sparse_features.shape),
+    )
+    return sparse_tensor
+
+
 class FFNN(nn.Module):
-    """construct Feed Forward Neural Network"""
     def __init__(self, input_size, hidden_sizes, output_size):
         super().__init__()
         torch.manual_seed(0)
-        # nn.Linear(input_size=tfidf_tensor.shape[1], hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
 
         self.input_size = input_size
         self.hidden_sizes = hidden_sizes
         self.output_size = output_size
-
         # input layer to the first hidden layer
         self.input_layer = nn.Linear(input_size, hidden_sizes[0])
-
         # multiple hidden layers
-        self.hidden_layers = nn.ModuleList(
-            [nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]) for i in range(len(hidden_sizes) - 1)])
-
+        self.hidden_layers = nn.ModuleList([nn.Linear(hidden_sizes[i], hidden_sizes[i+1]) for i in range(len(hidden_sizes)-1)])
         # last hidden layer
         self.output_layer = nn.Linear(hidden_sizes[-1], output_size)
+        self.leakyrelu = nn.LeakyReLU()
 
     def forward(self, x):
         # input layer to the first hidden layer
-        x = torch.relu(self.input_layer(x))
-
-        # transition between hidden layers
+        x = self.leakyrelu(self.input_layer(x))
+        # transition between hidden layers, use LeakyReLU as activation function
         for layer in self.hidden_layers:
-            x = torch.relu(layer(x))
-
+            x = self.leakyrelu(layer(x))
         # the last hidden layer to output
         x = self.output_layer(x)
 
@@ -58,40 +57,30 @@ class FFNN(nn.Module):
 
     def predict(self, X):
         Y_pred = self.forward(X)
-        print(Y_pred)
         return Y_pred
 
 
-def train_model(x, y, model, opt, loss_fn, batch_size=32, num_epochs=1000):
-    """train model with batch, but I think there are some bugs with the loss function part."""
+def train_model(x, y, model, opt, loss_fn, batch_size=32, epochs = 1000):
     dataset = torch.utils.data.TensorDataset(x, y)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    total_step = len(dataloader)
+
     for epoch in range(epochs):
         total_loss = 0.0
-
         for batch_x, batch_y in dataloader:
-            # Forward pass
-            logits = model(batch_x)
-
-            # Compute the loss
-            loss = loss_fn(logits, batch_y)
-
-            # Backward pass and optimization
-            loss.backward()
-            opt.step()
+            # Clear gradients of parameters
             opt.zero_grad()
-            # print('optimized')
-
+            # Forward pass
+            outputs = model(batch_x)
+            # Calculate Loss: softmax --> cross entropy loss
+            loss = loss_fn(outputs, batch_y)
+            loss.backward()
+            # Updating parameters
+            opt.step()
             total_loss += loss.item()
-
         # Average loss for the epoch
         average_loss = total_loss / len(dataloader)
-        if (epoch + 1) % 100 == 0:
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                  .format(epoch + 1, num_epochs, epoch + 1, total_step, average_loss))
-
-    return average_loss
+        if (epoch+1) % 100 == 0:
+            print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, epochs, average_loss))
 
 
 
