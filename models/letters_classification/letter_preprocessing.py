ce338c88f83b8d2604e03a5d2cb5e41bbf10caec
uffelmann <S.Uffelmann@campus.lmu.de>
2023-12-27 19:46:34 +0100

    letter preprocessing update

--- models/letters_classification/letter_preprocessing.py
+++ models/letters_classification/letter_preprocessing.py
@@ -1,40 +1,43 @@
 import json
-import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-path_to_file = "/Users/sarahannauffelmann/desktop/01_WiSe23:24/Seminar Klassifizierung/Projekt FFNN/letters/classifier_data_train.jsonl"
+def load_dataset(path):
+    "load the text and label from dataset"
+    list_of_letters = []
+    with open(path, "r", encoding="utf-8") as file:
+        for item in file:
+            data = json.loads(item)
+            list_of_letters.append(data)
+        
+        instances = [letter["text"] for letter in list_of_letters]
+        labels_author = [letter["author"] for letter in list_of_letters]    # for author classification
+        labels_lang = [letter["lang"] for letter in list_of_letters]        # for language identification
 
-list_of_letters = []
-with open(path_to_file, "r") as file:
-  for item in file:
-    data = json.loads(item)
-    list_of_letters.append(data)
+    return instances, labels_author, labels_lang
 
-num_letters = len(list_of_letters)
 
-# Check what an instance looks like:
-#print(list_of_letters[0])
+def create_sparse_vectors(instances, max_docfreq, min_docfreq):
+    # https://scikit-learn.org/stable/auto_examples/text/plot_document_classification_20newsgroups.html#sphx-glr-auto-examples-text-plot-document-classification-20newsgroups-py
+    "create tf-idf vector representations, instances: list of str"
+    vectorizer = TfidfVectorizer(max_df=max_docfreq, min_df=min_docfreq) # no stopwords filter as we are dealing with a variety of languages 
+    X_sparse = vectorizer.fit_transform(instances)
+    return X_sparse
 
-def get_vocab(list_of_letters):
-  vocab = set()
-  for l in list_of_letters:
-    text = l["text"]
-    tokens = nltk.word_tokenize(text)
-    for t in tokens:
-      vocab.add(t)
-  return vocab
 
-# Check vocab for all letters:
-letter_vocab = get_vocab(list_of_letters)
-#print(get_vocab(list_of_letters))
+if __name__ == '__main__':
+    # paths to files
+    path_to_train_file = "/Users/sarahannauffelmann/desktop/01_WiSe23:24/Seminar Klassifizierung/Projekt FFNN/letters/classifier_data_train.jsonl"
+    path_to_test_file = "/Users/sarahannauffelmann/desktop/01_WiSe23:24/Seminar Klassifizierung/Projekt FFNN/letters/classifier_data_eval.jsonl"
+    
+    # training data
+    train_inst, train_author_labels, train_lang_labels = load_dataset(path_to_train_file)
+    
+    # test data
+    test_inst, test_author_labels, test_lang_labels = load_dataset(path_to_test_file)
 
-# https://scikit-learn.org/stable/auto_examples/text/plot_document_classification_20newsgroups.html#sphx-glr-auto-examples-text-plot-document-classification-20newsgroups-py
-
-# create_sparse_vectors
-vectorizer = TfidfVectorizer(max_df=0.5, min_df=5) # should add stopwords
-texts = [letter["text"] for letter in list_of_letters]
-X_sparse = vectorizer.fit_transform(texts)
-
-#print(X_sparse)
-
+    # sparse vectors
+    train_vectors_sparse = create_sparse_vectors(train_inst, 0.5, 5)
+    test_vectors_sparse = create_sparse_vectors(test_inst, 0.5, 5)
+    #print(train_vectors_sparse)
+    #print(test_vectors_sparse)
