f94409f6a678e6e240d02c9b14280d87ed24b305
uffelmann <S.Uffelmann@campus.lmu.de>
2024-01-04 17:45:09 +0100

    author classification done

--- models/letters_classification/author_classification_model.py
+++ models/letters_classification/author_classification_model.py
@@ -5,6 +5,80 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+
+# Define the Feedforward Neural Network model
+class FFNN(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
+        super(FFNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.batch_norm = nn.BatchNorm1d(hidden_size1)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.batch_norm2 = nn.BatchNorm1d(hidden_size2)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(hidden_size2, output_size)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.batch_norm(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.batch_norm2(x)
+        x = self.relu2(x)
+        x = self.dropout2(x)
+        x = self.fc3(x)
+        return x
+
+def train_model(dataloader, model, optimizer, loss_func):
+    running_loss = 0.0
+
+    # Wrap train_loader with tqdm for the progress bar
+    data = tqdm(dataloader, desc='Training', leave=False)
+
+    for batch in data:
+        inputs = batch[0]
+        labels = batch[1]
+
+        optimizer.zero_grad() 
+
+        outputs = model(inputs)
+        loss = loss_func(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    
+    return running_loss / len(dataloader)
+
+
+def evaluate_model(model, test_data):
+    model.eval()
+    all_preds = []
+    all_labels = []
+
+    with torch.no_grad():
+        for batch in test_data:
+            inputs = batch[0]
+            labels = batch[1]
+
+            outputs = model(inputs)
+            pred = torch.argmax(outputs, 1)
+            #x, preds = torch.max(outputs, 1)
+            #print("argmax:", pred)
+            #print("max:", x, preds)
+
+            all_preds.extend(pred.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+    accuracy = accuracy_score(all_labels, all_preds)
+
+    return accuracy
+# _____________________________________________________________
 
  # paths to files
 path_to_train_file = "/Users/sarahannauffelmann/desktop/01_WiSe23:24/Seminar Klassifizierung/Projekt FFNN/letters/classifier_data_train.jsonl"
@@ -32,55 +106,29 @@ y_test = torch.tensor(labels_encoded_test)
 # create Dataloader
 # https://machinelearningmastery.com/training-a-pytorch-model-with-dataloader-and-dataset/
 dataloader_train = DataLoader(list(zip(X_train, y_train)), shuffle=True, batch_size=64)
-# for X_batch, y_batch in dataloader_train:
-#     print(X_batch, y_batch)
-
-# ---------------------------------------------
-
-# Define the Feedforward Neural Network model
-class FFNN(nn.Module):
-    def __init__(self, input_size, hidden_size, hidden_size2, output_size):
-        super(FFNN, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.batch_norm = nn.BatchNorm1d(hidden_size)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(hidden_size, hidden_size2)
-        self.batch_norm2 = nn.BatchNorm1d(hidden_size2)
-        self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(hidden_size2, output_size)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.batch_norm(x)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.batch_norm2(x)
-        x = self.relu2(x)
-        x = self.dropout2(x)
-        x = self.fc3(x)
-        return x
-
-def train_model(X, Y, model, opt, loss_func, batch_size, num_epochs):
-    pass
+dataloader_test = DataLoader(list(zip(X_test, y_test)), shuffle=True, batch_size=64)
 
 
-# Code von Vertiefung
-# train the model
-# number_of_epochs = 2
-# cbow_model = CBOW(emb_dim=embed_dim, vocab_dim=len(word2idx))
-# optimizer = torch.optim.Adam(cbow_model.parameters(), lr=0.001)
+#for X_batch, y_batch in dataloader_train:
+    #print(X_batch, y_batch)
 
-# use dataloader to shuffle the data!!!
-    # running_loss = 0.0
-    # for epoch in range(number_of_epochs):
-    #     #for i, data in tqdm(enumerate(input_data), total=len(list(input_data)), desc="Training"):
-    #     for i, data in tqdm(enumerate(input_data)):
-    #         instance_vector, label = data
-    #         optimizer.zero_grad()
-    #         output = cbow_model.forward(instance_vector)
-    #         loss = cbow_model.loss(output, label)
-    #         loss.backward() # stores the gradients of each parameter, can be called with grad()
-    #         optimizer.step() # will use those stored gradients to optimize weights
+# model parameters
+input_size = X_train.shape[1]
+output_size = 7         #  7 different authors
+hidden1 = 512
+hidden2 = 512
+
+model = FFNN(input_size, hidden1, hidden2, output_size)
+
+# training parameters
+num_epochs = 20
+learning_rate = 0.001
+loss_func = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# training loop
+for epoch in range(num_epochs):
+     train_loss = train_model(dataloader_train, model, optimizer, loss_func)
+     print(f'Epoch {epoch+1}, Training Loss: {train_loss:.4f}')
+
+print(evaluate_model(model, dataloader_test))
